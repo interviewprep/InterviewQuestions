8f444279c45291e6031d7499f5060d170d39b33a
interviewprep <interviewstudygroup@gmail.com>
2017-03-10 15:39:34 -0800

    add question

--- arrays/SubArrayofSize3.java
+++ arrays/SubArrayofSize3.java
@@ -1,10 +1,14 @@
+/*
+ * Calculate the sum of the largest possible subarray of size 3. 
+ */
+
 package arrays;
 
 public class SubArrayofSize3 {
 	public static int largestSum(int []A){
 		if(A==null||A.length<3) return 0;
 		int curSum=A[0]+A[1]+A[2],maxSum=curSum;
-		for(int start=0,end=3;end<A.length;start++,end++){
+		for(int start=0,end=3; end<A.length; start++,end++){
 			curSum=curSum-A[start]+A[end];
 			if(curSum>maxSum) maxSum=curSum;
 		}
